1028a12efcedb40b4d3e94bf4f336436da39d497
zhangyiming <zym@akka.co>
2016-06-30 14:09:12 +0800

    创建链式列表

---
+++ LinearList/LinearList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinearList
+//
+//  Created by akka on 16/6/30.
+//  Copyright © 2016年 akka. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
