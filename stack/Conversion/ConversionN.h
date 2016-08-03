b8576fdec30d9c58ef9b3bec94ab489d7c4acf35
zhangyiming <zym@akka.co>
2016-08-03 20:11:35 +0800

    十进制转化八进制

---
+++ stack/Conversion/ConversionN.h
@@ -0,0 +1,12 @@
+//
+// Created by akka on 16/8/3.
+//
+
+#ifndef CONVERSION_CONVERSION_H
+#define CONVERSION_CONVERSION_H
+
+#include "SequenceStack.h"
+#include <stdio.h>
+void conversioNum(int i);
+
+#endif //CONVERSION_CONVERSION_H
---
+++ stack/Conversion/main.c
@@ -0,0 +1,11 @@
+//
+// Created by akka on 16/8/3.
+//
+
+#include <stdio.h>
+#include "ConversionN.h"
+
+int main(int argc, const char * argv[]) {
+    int i = 23453;
+    conversioNum(i);
+}
